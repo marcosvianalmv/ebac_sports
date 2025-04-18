1cc7c7add110ff2b6f7cc7ee040f334095cdbaf0
Marcos Viana <marcosviana.lmv@gmail.com>
2025-04-17 23:37:27 -0300

    Faltando aplicar Redux Toolkit Query

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux'
 import * as S from './styles'
 
 import { Produto } from '../../App'
@@ -6,12 +7,15 @@ import cesta from '../../assets/cesta.png'
 import { paraReal } from '../Produto'
 
 type Props = {
-  itensNoCarrinho: Produto[]
   favoritos: Produto[]
 }
 
-const Header = ({ itensNoCarrinho, favoritos }: Props) => {
-  const valorTotal = itensNoCarrinho.reduce((acc, item) => {
+import { RootReducer } from '../../store'
+
+const Header = ({ favoritos }: Props) => {
+  const itens = useSelector((state: RootReducer) => state.carrinho.itens)
+
+  const valorTotal = itens.reduce((acc, item) => {
     acc += item.preco
     return acc
   }, 0)
@@ -23,7 +27,7 @@ const Header = ({ itensNoCarrinho, favoritos }: Props) => {
         <span>{favoritos.length} favoritos</span>
         <img src={cesta} />
         <span>
-          {itensNoCarrinho.length} itens, valor total: {paraReal(valorTotal)}
+          {itens.length} itens, valor total: {paraReal(valorTotal)}
         </span>
       </div>
     </S.Header>
---
+++ src/store/reducers/favoritos.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { Produto } from '../../App'
+
+type FavoritosState = {
+  itens: Produto[]
+}
+
+const initialState: FavoritosState = {
+  itens: []
+}
+
+const favoritosSlice = createSlice({
+  name: 'favoritos',
+  initialState,
+  reducers: {
+    adicionarfavoritos: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+
+      if (state.itens.find((p) => p.id === produto.id)) {
+        alert('Produto já adicionado')
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const { adicionarfavoritos } = favoritosSlice.actions
+export default favoritosSlice.reducer
